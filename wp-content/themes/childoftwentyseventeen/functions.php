f81c4799142e0d54eacd6f6fda8065f68bb61a3c
Lakshmi <lakshmi.nair8@gmail.com>
2019-06-07 14:45:47 -0300

    change is style sheet

--- wp-content/themes/childoftwentyseventeen/functions.php
+++ wp-content/themes/childoftwentyseventeen/functions.php
@@ -1,4 +1,11 @@
 <?php
+
+function wpb_add_google_fonts() {
+
+	wp_enqueue_style( 'wpb-google-fonts','href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans&display=swap"', false ); }add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+
+
 function my_theme_enqueue_styles() {   
  $parent_style = 'twentyseventeen-style'; // This is 'twentythirteen-style' for the Twenty Fifteen theme.
  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );

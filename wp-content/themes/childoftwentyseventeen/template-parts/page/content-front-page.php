ec040c2c4b56a268341ec4f9bcbb043f6ede9c55
Lakshmi <lakshmi.nair8@gmail.com>
2019-06-21 15:19:23 -0300

    changed the side bar in home page

--- wp-content/themes/childoftwentyseventeen/template-parts/page/content-front-page.php
+++ wp-content/themes/childoftwentyseventeen/template-parts/page/content-front-page.php
@@ -28,9 +28,9 @@
 	<div class="panel-content">
 		<div class="wrap">
 			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				<!-- <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
-				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+				<?php twentyseventeen_edit_link( get_the_ID() ); ?> -->
 
 			</header><!-- .entry-header -->
 
